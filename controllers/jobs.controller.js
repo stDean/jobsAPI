1866a591f8ae58a6113315e0911416278dc36339
Toba-dean <tobaogundimu@gmail.com>
2023-05-14 19:57:35 +0100

    added all the job api

--- controllers/jobs.controller.js
+++ controllers/jobs.controller.js
@@ -1,20 +1,53 @@
+const { StatusCodes } = require('http-status-codes');
+
 const Job = require('../models/jobs.model');
+const { NotFoundError, BadRequestError } = require('../errors');
 
 const jobsController = {
-  createJob: async () => {
-    res.send("Create Job")
+  createJob: async (req, res) => {
+    req.body.createdBy = req.user.userId;
+    const job = await Job.create({ ...req.body });
+    res.status(StatusCodes.CREATED).json({ job });
   },
   getAllJobs: async (req, res) => {
-    res.send("All Jobs")
+    const { userId } = req.user;
+    const jobs = await Job.find({ createdBy: userId }).sort('createdAt');
+    res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
   },
   getJob: async (req, res) => {
-    res.send("Single Job")
+    const { params: { id: jobId }, user: { userId } } = req;
+    const job = await Job.findOne({ _id: jobId, createdBy: userId });
+    if (!job) {
+      throw new NotFoundError(`No job with id(${jobId}) found.`);
+    }
+    res.status(StatusCodes.OK).json({ job });
   },
   updateJob: async (req, res) => {
-    res.send("Update Jobs")
+    const {
+      params: { id: jobId },
+      user: { userId },
+      body: { company, position }
+    } = req;
+
+    if (company === '' && position === '') {
+      throw new BadRequestError('This fields cannot be empty.');
+    }
+
+    const job = await Job.findOneAndUpdate(
+      { _id: jobId, createdBy: userId },
+      req.body,
+      { new: true, runValidators: true }
+    );
+
+    if (!job) {
+      throw new NotFoundError(`No job with id(${jobId}) found.`);
+    }
+    res.status(StatusCodes.OK).json({ success: true, job });
   },
   deleteJob: async (req, res) => {
-    res.send("Delete Jobs")
+    const { params: { id: jobId }, user: { userId } } = req;
+    await Job.findOneAndDelete({ _id: jobId, createdBy: userId });
+    res.status(StatusCodes.OK).json({ success: true });
   }
 }
 
--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -4,7 +4,7 @@ const { UnauthenticatedError } = require('../errors');
 
 const AuthMiddleware = async (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith('Bearers')) {
+  if (!authHeader || !authHeader.startsWith('Bearer')) {
     throw new UnauthenticatedError("Invalid Authorization")
   }
 
